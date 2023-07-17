9d8bdd95375c7a4103b434eb36f995e1ccc495d8
Richard Chalk <barmybrit@gmail.com>
2023-07-17 15:38:33 +0200

    Final v2

--- IndianObserver/Program.cs
+++ IndianObserver/Program.cs
@@ -7,8 +7,10 @@ namespace IndianObserver
     {
         static void Main(string[] args)
         {
+            // Vi behöver en publisher
             var group = new Subject.Group();
 
+            // Skapa några observers
             // Bättre med en list?
             var users = new List<User>()
             {
@@ -17,14 +19,20 @@ namespace IndianObserver
                 new User(3)
             };
 
+            // Lägg till våra nya users som subscribers
             group.Subscribe(users[0]);
             group.Subscribe(users[1]);
             group.Subscribe(users[2]);
 
+            // Vår subject 'state' har ändrat (fått ett nytt meddelande)
+            // Så nu måste vi informera alla våra users
             group.Notify("First New message!");
 
+            // User1 är missnöjd.
             group.UnSubscribe(users[0]);
 
+            // Informera om ett nytt meddelande
+            // User1 är inte iunkluderat i listan längre :)
             group.Notify("Second New message!");
         }
     }
